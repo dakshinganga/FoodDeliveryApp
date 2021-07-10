4b0ebf11a7ed2ebe4e5d50159d35be150bc049d3
Dipendra <jaiswaldipendra12@gmail.com>
2021-07-10 08:57:13 +0530

    make home page

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const ejs = require('ejs')
 const expressLayout = require("express-ejs-layouts")
 const path = require('path')
 
-
+//Assets
+app.use(express.static('public'))
 app.get('/',(req,res)=>{
 
     res.render('home')
@@ -17,7 +18,7 @@ app.set('views',path.join(__dirname,'/resources/views'))
 app.set('view engine','ejs')
 
 
-const PORT = process.env.PORT || 3300
+const PORT = process.env.PORT || 31000
 app.listen(PORT,()=>{
     console.log(`Listening on port ${PORT}`)
 })
