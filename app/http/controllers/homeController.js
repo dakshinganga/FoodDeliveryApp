f8f5bed68aaba3740ad0fb7bf2194c11d61c8090
Dipendra <jaiswaldipendra12@gmail.com>
2022-01-15 14:07:48 +0530

    production builds

--- app/http/controllers/homeController.js
+++ app/http/controllers/homeController.js
@@ -4,7 +4,7 @@ function homeController() {
     return {
         async index(req, res) {
 
-            //const pizzas = await Menu.find()
+            const pizzas = await Menu.find()
            return res.render('home',{pizzas:pizzas})
 
             /*Menu.find().then(function(pizzas){
